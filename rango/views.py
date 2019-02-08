603cb2d7f9ae3815a1c2e037fa28d23eee3d3898
Zihao Liu <2379652L@student.gla.ac.uk>
2019-02-08 18:21:37 +0000

    chapter 5 exercise

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
     # return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
@@ -9,3 +10,8 @@ def index(request):
 def about(request):
     return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
 # Create your views here.
+
+# def index(request):
+#     category_list = Category.objects.order_by('-likes')[:5]
+#     context_dict = {'categories': category_list}
+#     return render(request, 'rango/index.html', context_dict)
